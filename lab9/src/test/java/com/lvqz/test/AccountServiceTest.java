868a8381a3fe0c043e7e735e9ebbe3dc09502a99
lvqz <qiuzhen_1726@163.com>
2020-05-05 20:07:31 +0800

    更新lab9  重温AOP开始咯

---
+++ lab9/src/test/java/com/lvqz/test/AccountServiceTest.java
@@ -0,0 +1,29 @@
+package com.lvqz.test;
+
+
+import com.lvqz.spring.lab9.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author: lvqz
+ * @date: 2020/4/29
+ * @time: 16:23
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService accountService;
+
+    @Test
+    public void transfer() {
+
+        accountService.transferAccount("aaa","bbb",100f);
+
+    }
+}
