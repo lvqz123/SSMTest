be33a43a9fa9ceca6484c49ba8dd4cda7fdaffab
吕秋圳 <lvqz@allinfinance.com>
2020-05-28 16:59:11 +0800

    更新lab20，配置文件改为读取properties，优雅脱敏

--- lab20/src/test/java/com/lvqz/test/MybatisTest.java
+++ lab20/src/test/java/com/lvqz/test/MybatisTest.java
@@ -18,22 +18,38 @@ import java.util.List;
  */
 public class MybatisTest {
 
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) {
         //1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis.xml");
-        //2.创建SQLSessionFactory的构建者对象
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        //3.创建SqlSessionFactory对象
-        SqlSessionFactory factory = builder.build(in);
-        //4.使用SqlSession创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
-        //5.使用SqlSession创建Dao对象
-        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
-        //6.调用IUserDao的方法
-        List<User> users = userDao.findAllUser();
-        for (User user : users) {
-            System.out.println(user);
+        InputStream in = null;
+        SqlSession sqlSession = null;
+        try {
+            in = Resources.getResourceAsStream("mybatis.xml");
+            //2.创建SQLSessionFactory的构建者对象
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            //3.创建SqlSessionFactory对象
+            SqlSessionFactory factory = builder.build(in);
+            //4.使用SqlSession创建SqlSession对象
+            sqlSession = factory.openSession();
+            //5.使用SqlSession创建Dao对象
+            IUserDao userDao = sqlSession.getMapper(IUserDao.class);
+            //6.调用IUserDao的方法
+            List<User> users = userDao.findAllUser();
+            for (User user : users) {
+                System.out.println(user);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+            try {
+                if( in != null)
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
+
 
     }
 
